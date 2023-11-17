644392999a9ec41bb4978a0394f5a341b9174f52
xvrongkang <xvrongkang@outlook.com>
2023-11-17 16:39:44 +0800

    update

--- src/api/demo/index.ts
+++ src/api/demo/index.ts
@@ -1,8 +1,8 @@
-import http from '~/utils/http';
+import request from '~/utils/request';
 
 export const get_demo = async (param_value: any) => {
     try {
-        const response = await http.get("/get_demo", { params: { param_name: param_value } });
+        const response = await request.get("/get_demo", { params: { param_name: param_value } });
         return response;
     } catch (error) {
         console.log(error);
@@ -12,7 +12,7 @@ export const get_demo = async (param_value: any) => {
 export const post_demo = async (data: any) => {
     // data为json格式
     try {
-        const response = await http.post("/post_demo", data);
+        const response = await request.post("/post_demo", data);
         return response;
     } catch (error) {
         console.log(error);
